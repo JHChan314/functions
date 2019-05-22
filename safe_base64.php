8ea2488ff1179f0a720a9a0af25e52c978b7c410
JHChan314 <2276393+JHChan314@users.noreply.github.com>
2019-05-22 16:13:51 +0800

    Create safe_base64.php

---
+++ safe_base64.php
@@ -0,0 +1,29 @@
+<?php
+    /**
+     * URL base64解码
+     * '-' -> '+'
+     * '_' -> '/'
+     * 字符串长度%4的余数，补'='
+     * @param unknown $string
+     */
+    function urlsafe_b64decode($string) {
+        $data = str_replace(array('-','_'),array('+','/'),$string);
+        $mod4 = strlen($data) % 4;
+        if ($mod4) {
+            $data .= substr('====', $mod4);
+        }
+        return base64_decode($data);
+    }
+
+    /**
+     * URL base64编码
+     * '+' -> '-'
+     * '/' -> '_'
+     * '=' -> ''
+     * @param unknown $string
+     */
+    function urlsafe_b64encode($string) {
+        $data = base64_encode($string);
+        $data = str_replace(array('+','/','='),array('-','_',''),$data);
+        return $data;
+    }
